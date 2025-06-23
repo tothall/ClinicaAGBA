f4fcf6b3d5dfc1063a8db109ab41ad274dda43a3
Lucas <100217523+tothall@users.noreply.github.com>
2025-06-22 23:49:15 -0300

    add teste para o teste do repositório de médicos

---
+++ teste/TesteRepositorios.java
@@ -0,0 +1,32 @@
+package booktour;
+import java.util.ArrayList;
+public class Teste {
+    public static void main(String[] args) {
+        Medico med1 = new Medico("123");
+        Medico med2 = new Medico("124");
+        Medico med3 = new Medico("125");
+
+        RepositorioMedico repositorio_medico = new RepositorioMedico();
+        repositorio_medico.adicionar(med1);
+        repositorio_medico.adicionar(med2);
+        repositorio_medico.adicionar(med3);
+        
+        repositorio_medico.remover(med3);
+
+        for (Medico medico: repositorio_medico.getMedicos()){
+            System.out.println(medico);
+        }
+        
+        ArrayList<Medico> medicos = repositorio_medico.listar();
+        for (Medico medico: medicos){
+            System.out.println(medico);
+        }
+        
+        System.out.println(repositorio_medico.buscar(med1));
+        repositorio_medico.atualizar(med1, med2);
+        
+        for (Medico medico: medicos){
+            System.out.println(medico);
+        }
+}
+}
